2c834f6bc254e1d2d099717ba16ff4a34d3a674f
holeman79 <holeman79@gmail.com>
2018-09-09 17:41:05 +0900

    test

--- src/main/java/com/appvengers/seoulapp/domain/user/UserService.java
+++ src/main/java/com/appvengers/seoulapp/domain/user/UserService.java
@@ -1,5 +1,7 @@
 package com.appvengers.seoulapp.domain.user;
 
+import java.time.LocalDate;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,7 +14,11 @@ public class UserService {
 	
 	public boolean loginOrJoin(User user) {
 		User findUser = userRepository.findByUserId(user.getUserId());
-		if(findUser == null) userRepository.save(user);
+		if(findUser == null) {
+			user.setRegDt(LocalDate.now());
+			user.setUpdDt(LocalDate.now());
+			userRepository.save(user);
+		}
 		return (findUser == null) ? false : true;
 	}
 }
