219d68c65177f70803b8ffc960c8b75cb3ced3f5
holeman79 <holeman79@gmail.com>
2018-09-15 20:47:06 +0900

    최신 업데이트

--- src/main/java/com/appvengers/seoulapp/domain/user/User.java
+++ src/main/java/com/appvengers/seoulapp/domain/user/User.java
@@ -18,7 +18,7 @@ import lombok.Getter;
 public class User {
 	
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	//@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private String userId;
 	
 	private String userName;
