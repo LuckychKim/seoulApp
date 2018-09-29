769ad0ea96864862a6bac8261bb4750ecf8137aa
holeman79 <holeman79@gmail.com>
2018-09-29 12:16:36 +0900

    tourController update

--- src/main/java/com/appvengers/seoulapp/controller/TourController.java
+++ src/main/java/com/appvengers/seoulapp/controller/TourController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,8 +31,8 @@ public class TourController {
 		return tourService.retrieveTourDetailInfo();
 	}
 	
-	@GetMapping("/retrieveTour")
-	public Tour retrieveTourById( String tourId ) {
+	@GetMapping("/retrieveTour/{tourId}")
+	public Tour retrieveTourById(@PathVariable String tourId ) {
 		
 		return tourService.retrieveTourById(tourId);
 	}
