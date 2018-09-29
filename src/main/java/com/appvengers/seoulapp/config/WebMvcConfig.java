15944b73232b4ed72b02ad66e909cdf5302a540c
LuckychKim <luckych1025@gmail.com>
2018-09-29 11:26:10 +0900

    image 불러올 수 있도록 interceptor 설정

--- src/main/java/com/appvengers/seoulapp/config/WebMvcConfig.java
+++ src/main/java/com/appvengers/seoulapp/config/WebMvcConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.appvengers.seoulapp.interceptor.LogInterceptor;
@@ -15,6 +16,15 @@ public class WebMvcConfig implements WebMvcConfigurer {
 	@Autowired
 	private LogInterceptor interceptor;
 
+	private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+            "classpath:/resources/", "classpath:/static/", "classpath:/public/" };
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+            .addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+    }
+    
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(interceptor)
